d6a6a29b2309be5aa7f04c542c76967b82046ef7
smileypiyali2024 <smileypiyali2024@gmail.com>
2026-01-24 12:09:54 +0530

    coupon updated code added

--- controllers/coupon.controller.js
+++ controllers/coupon.controller.js
@@ -56,6 +56,58 @@ export const getCoupon = async (req, res) => {
     }
 }
 
+export const updateCoupon = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { 
+            code, 
+            type, 
+            discount, 
+            expiry_date, 
+            usage_limit, 
+            used_count, 
+            min_purchase 
+        } = req.body || {};
+
+        if (!code || code.trim() === "") {
+            return res.status(400).json({ message: "Coupon code is required" });
+        }
+
+        if (!expiry_date || isNaN(new Date(expiry_date))) {
+            return res.status(400).json({ message: "Coupon expire date is required" });
+        }
+
+        const coupon = await Coupon.findById(id);
+
+        if (!coupon) {
+            return res.status(404).json({ message: "Coupon is not found" });
+        }
+
+        coupon.code = code ?? coupon.code;
+        coupon.type = type ?? coupon.type;
+        coupon.discount = discount ?? coupon.discount;
+        coupon.expiry_date = new Date(expiry_date) ?? coupon.expiry_date;
+        coupon.usage_limit = Number(usage_limit) ?? coupon.usage_limit;
+        coupon.used_count = Number(used_count) ?? coupon.used_count;
+        coupon.min_purchase = min_purchase ?? coupon.min_purchase;
+
+        await coupon.save();
+
+        return res.status(200).json({ 
+            message: "Coupon successfully updated", 
+            status: "success",
+            data: coupon
+        });
+        
+    } catch (error) {
+        console.error("Coupon update error:", error.message);
+        return res.status(500).json({ 
+            message: error.message, 
+            status: "error" 
+        });
+    }
+}
+
 export const deleteCoupon = async (req, res) => {
     try {
         const { id } = req.params;
--- routes/coupon.routes.js
+++ routes/coupon.routes.js
@@ -1,5 +1,5 @@
 import express from "express"; // Import the Express library
-import { createCoupon, deleteCoupon, getAllCoupons, getCoupon } from "../controllers/coupon.controller.js";
+import { createCoupon, deleteCoupon, getAllCoupons, getCoupon, updateCoupon } from "../controllers/coupon.controller.js";
 import { isAdmin } from "../middlewares/admin.middleware.js";
 import { protect } from "../middlewares/auth.middleware.js";
 const router = express.Router(); // Create a new router object / Instance of the Express Router
@@ -9,6 +9,8 @@ router.get("/coupons", protect, isAdmin, getAllCoupons); // verifies token, atta
 router.post("/create-coupon", protect, isAdmin, createCoupon);
 
 router.get("/coupon/:id", protect, isAdmin, getCoupon);
+
+router.put("/coupon/update/:id", protect, isAdmin, updateCoupon);
 
 router.delete("/coupon-delete/:id", protect, isAdmin, deleteCoupon);
 
