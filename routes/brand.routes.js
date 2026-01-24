b6e24810a5cdd71ff015e04713b85238c565e8f7
smileypiyali2024 <smileypiyali2024@gmail.com>
2026-01-24 13:17:37 +0530

    brand updated code added

--- routes/brand.routes.js
+++ routes/brand.routes.js
@@ -1,5 +1,5 @@
 import express from "express"; // Import the Express library
-import { createBrand, deleteBrand, getAllBrands, getBrand } from "../controllers/brand.controller.js";
+import { createBrand, deleteBrand, getAllBrands, getBrand, updateBrand } from "../controllers/brand.controller.js";
 import { isAdmin } from "../middlewares/admin.middleware.js";
 import { protect } from "../middlewares/auth.middleware.js";
 import { createUploadMiddleware } from "../middlewares/upload.middleware.js";
@@ -12,6 +12,8 @@ router.get("/brands", protect, isAdmin, getAllBrands);
 router.post("/create-brand", protect, isAdmin, uploadBrandImage.single('brand_image'), createBrand);
 
 router.get("/brand/:id",  protect, isAdmin, getBrand);
+
+router.put("/brand/update/:id",  protect, isAdmin, updateBrand);
 
 router.delete("/brand-delete/:id", protect, isAdmin, deleteBrand);
 
