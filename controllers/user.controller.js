0f44b3e7887c420234851395af59d64afaced08a
addasamit <amit94.developer@gmail.com>
2026-03-02 23:56:42 +0530

    code updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
     if (!user) {
       return res.status(401).json({
         success: false,
-        message: "Invalid credentials",
+        message: "User not found",
       });
     }
 
